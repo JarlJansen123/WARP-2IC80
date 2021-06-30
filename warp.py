f9e109ec068cb989648f2829058226b844aedf21
RobertsLipsnis <44003194+RobertsLipsnis@users.noreply.github.com>
2021-06-30 09:20:53 +0300

    Add files via upload

--- warp.py
+++ warp.py
@@ -1,10 +1,15 @@
 from scapy.all import *
 from arp import arp
 from dnsfinal import dnsfinal
+from Tkinter import*
 import threading
 
-print("--=====-- Welcome to WARP, ARP/DNS poisoning tool --=====--")
-time.sleep(0.5)
+root = Tk()
+root.geometry('800x800')
+root.configure(background='#F0F8FF')
+root.title('ARP_DNS_TOOL')
+
+
 
 #variables
 #selected mode that the program will perform (arp or dns)
@@ -12,41 +17,62 @@ mode = ""
 #selected interface to use
 interface = ""
 
+def initiate_attack():
+    proc_thread = None
+    if mode == "arp":
+        process = arp(interface, root)
+        process.getInput()
+    elif mode == "dns":
+        process = dnsfinal(interface, root)
+        process.getInput()
+    else:
+        print("BAD INITIATE")
 
-#selecting procedure to execute
-while(mode != "dns" and mode != "arp"):
-    mode = raw_input("Select mode by typing arp or dns: ")
-    if(mode != "dns" and mode != "arp"):
-        print("Enter a valid input!")
 
-#selecting interface to use
-print("Available interfaces: ")
-# print the list of available interfaces and print them
-print(get_if_list())
-#user enters which interface to use
-while(not(interface in get_if_list())):
-    interface = raw_input("Select the interface that you want to use: ")
-    if(not(interface in get_if_list())):
-        print("Enter a valid input!")
+def int_selection(modeType):
+    global interface
+    interface = modeType
+    for widget in root.winfo_children():
+        widget.destroy()
+    initiate_attack()
 
-#calling the process we want to run
-proc_thread = None
-if(mode == "arp"):
-    process = arp(interface)
-    process.getInput()
-    proc_thread = threading.Thread(target=process.startProcess)
-    proc_thread.daemon = True
-    proc_thread.start()
-elif(mode == "dns"):
-    process = dnsfinal(interface)
-    process.getInput()
-    process.startProcess()
 
-end_thread = raw_input("Press enter to stop the thread that is currently running")
-print("stopping the poisoning")
-proc_thread.join()
-if proc_thread.is_alive():
-    print("HELP IT IS NOT WORKING")
-else:
-    print("IT WORKS")
+def select_mode():
+    Label(root, text='Select the interface that you want to use').pack()
+    OPTIONS = get_if_list()  # etc
+    selectMode = Listbox(root, selectmode="single", width=100)
+    for each_item in range(len(OPTIONS)):
+        selectMode.insert(END, OPTIONS[each_item])
+    selectMode.pack()
 
+    def get_selection():
+        allItems = selectMode.get(0, END)  # tuple with text of all items in Listbox
+        selIdx = selectMode.curselection()  # tuple with indexes of selected items
+        selList = [allItems[item] for item in selIdx]
+        selectedMode = selList[0]
+        int_selection(selectedMode)
+
+    Button(root, text="Execute", command=get_selection).pack()
+
+
+
+def on_dns_button():
+    for widget in root.winfo_children():
+        widget.destroy()
+    global mode
+    mode = "dns"
+    select_mode()
+
+
+def on_arp_button():
+    for widget in root.winfo_children():
+        widget.destroy()
+    global mode
+    mode = "arp"
+    select_mode()
+
+entry = Label(root, text='Welcome to our tool. Please select the attack').place(relx=0.5, rely=0.02, anchor='center')
+dnsButton = Button(root, text="Execute DNS attack", command=on_dns_button).place(relx=0.6, rely=0.1, anchor='center')
+arpButton = Button(root, text="Execute ARP poisoning", command=on_arp_button).place(relx=0.4, rely=0.1, anchor='center')
+
+root.mainloop()
